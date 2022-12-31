9167299ee2cfe257969b016ebbdca8661639860e
Yixin <1203694318@qq.com>
2022-12-31 21:41:57 +0800

    Add source code by cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout << "In the entrance examination 
+    I performed awfully bad.\n " << 
+    "And I know I must fail agin\n" <<
+    "What a tragedy I wrote in the past three years :-(\n"
+
+    return 0;
+}
